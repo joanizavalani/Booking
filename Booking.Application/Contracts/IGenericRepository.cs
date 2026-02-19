7c6696ec0f2232ab10ccb0387967492975139e94
Joani Zavalani <joanizavalani@gmail.com>
2026-02-19 22:51:03 +0100

    added IGenericRepository (Application) and GenericRepository (Infrastructure)

---
+++ Booking.Application/Contracts/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Booking.Application.Contracts;
+
+public interface IGenericRepository<TEntity>
+    where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(Guid id);
+
+    Task<List<TEntity>> GetAllAsync();
+
+    Task AddAsync(TEntity entity);
+
+    void Update(TEntity entity);
+
+    void Delete(TEntity entity);
+}
---
+++ Booking.Infrastructure/Contracts/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using Booking.Application;
+using Microsoft.EntityFrameworkCore;
+
+namespace Booking.Infrastructure.Contracts;
+
+public class GenericRepository<TEntity>
+    : IGenericRepository<TEntity>
+    where TEntity : class
+{
+    protected readonly BookingDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    public GenericRepository(BookingDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task<TEntity?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<List<TEntity>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task AddAsync(TEntity entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public void Update(TEntity entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void Delete(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
